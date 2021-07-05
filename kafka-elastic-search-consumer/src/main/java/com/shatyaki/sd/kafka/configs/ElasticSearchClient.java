7a7055bba48701ae2504574b34c94fd67959aa1e
Shatyaki Dutt <shatyaki_dutt@hotmail.com>
2021-07-05 18:41:18 +0530

    Make consumer idempotent

--- kafka-elastic-search-consumer/src/main/java/com/shatyaki/sd/kafka/configs/ElasticSearchClient.java
+++ kafka-elastic-search-consumer/src/main/java/com/shatyaki/sd/kafka/configs/ElasticSearchClient.java
@@ -1,6 +1,9 @@
 package com.shatyaki.sd.kafka.configs;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -8,6 +11,9 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -18,22 +24,21 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.JsonParser;
+
 public class ElasticSearchClient {
 
-    
     public static RestHighLevelClient createElasticSearchClient() {
-        
+
         String hostname = ""; // localhost or bonsai url
         String username = ""; // needed only for bonsai
         String password = ""; // needed only for bonsai
 
         // credentials provider help supply username and password
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(username, password));
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
-        RestClientBuilder builder = RestClient.builder(
-                new HttpHost(hostname, 443, "https"))
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443, "https"))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
                     public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
@@ -44,23 +49,51 @@ public class ElasticSearchClient {
         RestHighLevelClient client = new RestHighLevelClient(builder);
         return client;
     }
-    
-    
-    public static void main(String ...agrs) throws IOException {
+
+    public static void main(String... agrs) throws IOException, InterruptedException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchClient.class);
+
+//        Kafka consumer 
+        KafkaConsumerConfig consumerConfig = new KafkaConsumerConfig();
+        Properties kafakaConsumerProperties = consumerConfig.getConsumerProperties("kafka-demo-app-1");
+
+        String topic = "twitter-tweets";
+
         RestHighLevelClient client = createElasticSearchClient();
-        String jsonObject = "{\"age\":10,\"dateOfBirth\":1471466076564,"
-                +"\"fullName\":\"John Doe\"}";
-        IndexRequest request = new IndexRequest("twitter").source(jsonObject, XContentType.JSON);
-        
-        IndexResponse resp = client.index(request, RequestOptions.DEFAULT);
-        String id = resp.getId();
-        logger.info("id : " + id);
-        
-        client.close();
-        
+
+//        IndexResponse resp = client.index(request, RequestOptions.DEFAULT);
+//        String id = resp.getId();
+//        logger.info("id : " + id);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(kafakaConsumerProperties);
+        consumer.subscribe(Collections.singleton(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record : records) {
+                // insert data into elastic search
+
+//                Generic kafka id 
+//                String messageId = record.topic() +"_"+ record.partition() +"_"+ record.offset();
+
+//                Twitter feed specific id 
+                String messageId = extractIdFromTweet(record.value());
+                IndexRequest request = new IndexRequest("twitter").id(messageId).source(String.valueOf(record.value()),
+                        XContentType.JSON);
+                IndexResponse resp = client.index(request, RequestOptions.DEFAULT);
+                String id = resp.getId();
+                logger.info("id : " + id);
+                Thread.sleep(1000);
+            }
+        }
+//        client.close();
+
     }
-    
+
+    private static String extractIdFromTweet(String tweet) {
+        JsonParser jsonParser = new JsonParser();
+        return jsonParser.parse(tweet).getAsJsonObject().get("id_str").getAsString();
+    }
+
 }
-
-
---
+++ kafka-elastic-search-consumer/src/main/java/com/shatyaki/sd/kafka/configs/KafkaConsumerConfig.java
@@ -0,0 +1,23 @@
+package com.shatyaki.sd.kafka.configs;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class KafkaConsumerConfig {
+        public Properties getConsumerProperties(String groupId) {
+            Properties properties = new Properties();
+            String bootStrapServers = "localhost:9092";
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    //        Earliest - Reads from every first of topic
+    //        Latest - Read only new messages onwards
+    //        None - Throws error
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            return properties;
+        }
+}
+
